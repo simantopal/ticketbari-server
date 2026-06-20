9159e51b20c208f550562fd5514a4b42a1b75308
simantopal <simantop13@gmail.com>
2026-06-20 14:26:09 +0600

    update api function added in the my tickets card

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 5000
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -33,24 +33,74 @@ async function run() {
     const ticketCollection = database.collection("tickets");
 
 
-    app.get('/api/tickets', async(req, res) =>{
-        const query = {};
-        if(req.query.vendorEmail){
-            query.vendorEmail = req.query.vendorEmail;
-        }
-        if(req.query.status){
-            query.status = req.query.status;
-        }
-        const cursor = ticketCollection.find(query);
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/api/tickets', async (req, res) => {
+      const query = {};
+      if (req.query.vendorEmail) {
+        query.vendorEmail = req.query.vendorEmail;
+      }
+      if (req.query.status) {
+        query.status = req.query.status;
+      }
+      const cursor = ticketCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.post('/api/tickets', async(req, res) =>{
-        const ticket = req.body;
-        const result = await ticketCollection.insertOne(ticket);
-        res.send(result);
-    })
+
+    // Get Single Ticket
+    app.get("/api/tickets/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await ticketCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      res.send(result);
+    });
+
+    // Add Ticket
+    app.post("/api/tickets", async (req, res) => {
+      const ticket = req.body;
+
+      const result = await ticketCollection.insertOne({
+        ...ticket,
+        status: "pending",
+        createdAt: new Date(),
+      });
+
+      res.send(result);
+    });
+
+    // Update Ticket
+    app.put("/api/tickets/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedTicket = req.body;
+
+      const result = await ticketCollection.updateOne(
+        {
+          _id: new ObjectId(id),
+        },
+        {
+          $set: {
+            ...updatedTicket,
+            updatedAt: new Date(),
+          },
+        }
+      );
+
+      res.send(result);
+    });
+
+    // Delete Ticket
+    app.delete("/api/tickets/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result = await ticketCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+
+      res.send(result);
+    });
 
 
 
